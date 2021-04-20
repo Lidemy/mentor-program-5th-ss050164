248bed62a974a80cde988a3545ff508b56dd3089
keke <3352615ko@gmail.com>
2021-04-20 08:29:15 -0700

    rewrite hw2 & to re-push

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,8 @@
 function capitalize(str) {
-	str = str[0].toUpperCase() + str.slice(1)
+	if (str.charCodeAt(0) >= 97 && str.charCodeAt(0) <= 122) {
+		str = String.fromCharCode(str.charCodeAt(0) - 32) + str.slice(1)
+	}
+	//str = str[0].toUpperCase() + str.slice(1)
 	return str  
 }
 
