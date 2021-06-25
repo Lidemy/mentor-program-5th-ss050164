bfb1c7534a3186c268d3b4b451637930ef5ea09c
keke <3352615ko@gmail.com>
2021-06-25 09:08:47 -0700

    week 11- modified hw 1 handle_add_comment and removed unnesceesary files

--- homeworks/week11/hw1/handle_add_comment.php
+++ homeworks/week11/hw1/handle_add_comment.php
@@ -1,13 +1,26 @@
 <?php
   session_start();
   require_once("conn.php");
+  require_once("utils.php");
 
+  $username = NULL;
+  if (!$_SESSION['username']) {
+    header("Location: index.php?errCode=3");
+    die("Access denied!");
+  }
+
+  $username = $_SESSION['username'];
+  $user = get_user_from_username($username);
+  if ($user['role_id'] === 3) {
+    header("Location: index.php?errCode=3");
+    die("Access denied!");    
+  }
+  
   if (empty($_POST['content'])) {
     header("Location: index.php?errCode=1");
     die("Data incomplete!");
   }
-
-  $username = $_SESSION['username'];
+  
   $content = $_POST['content'];
 
   $sql = "INSERT INTO keke_comments (username, content) VALUES (?, ?)";
